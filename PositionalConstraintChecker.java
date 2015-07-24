1ee95b87e7ccb986d158e7b78f57028fa09c2246
PoojaSB93 <poojasb2093@gmail.com>
2015-07-24 12:31:50 +0530

    Checks for the words that satisfy the positional constraints.

---
+++ PositionalConstraintChecker.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class PositionalConstraintChecker {
+	{
+		String[] listofwords,validwords;
+		
+	   public PositionalConstraintChecker(String[] wordlist) {
+		   
+		   for(int i=0; i<wordlist.length; i++) {
+			   listofwords[i] = wordlist[i];
+		   }
+	   public static boolean PositionChecker( ) {
+		   for(int i =0;i<listofwords.length;i++) {
+			   
+			   
+		   }
+		   
+	   }
+	   
+	   
+	   }
+	   
+		
+	}
