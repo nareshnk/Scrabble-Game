c2aec484195ab5fa5993ba6c3e276f5006a162b2
Sonia V Mathew <sonisvm@gmail.com>
2015-07-24 13:49:56 +0530

    function to check a constraint given a list of words

--- PositionalConstraintChecker.java
+++ PositionalConstraintChecker.java
@@ -1,24 +1,31 @@
 import java.io.*;
 import java.util.*;
 public class PositionalConstraintChecker {
-	{
-		String[] listofwords,validwords;
-		
-	   public PositionalConstraintChecker(String[] wordlist) {
-		   
-		   for(int i=0; i<wordlist.length; i++) {
-			   listofwords[i] = wordlist[i];
-		   }
-	   public static boolean PositionChecker( ) {
-		   for(int i =0;i<listofwords.length;i++) {
+	
+		  
+	   public static ArrayList ConstraintChecker(String[] wordList, String constraint) {
+		  
+		   char letter;
+		   int position;
+		   boolean failed = false;
+		   List validWords = new ArrayList();
+		   for (int i = 0;i < wordList.length; i++) {
+			  
+			   for (int j = 0;j < constraint.length();j += 2){
+				   letter = constraint.charAt(j);
+				   position = constraint.charAt(j + 1);
+				   if (letter != wordList[i].charAt(position)){
+					   failed = true;
+					   break;
+				   }
+			   }
+			   if (failed != true){
+				   validWords.add(wordList[i]);
+			   }
 			   
-			   
-		   }
-		   
-	   }
-	   
-	   
-	   }
+		  }
+		   return validWords;
 	   
 		
 	}
+	   }
