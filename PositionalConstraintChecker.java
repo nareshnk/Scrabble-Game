942bdc8b96455347fa45a4661bb54908e2c746ef
PoojaSB93 <poojasb2093@gmail.com>
2015-07-24 14:36:41 +0530

    PositionConstraintChecker with main function for testing

--- PositionalConstraintChecker.java
+++ PositionalConstraintChecker.java
@@ -1,31 +1,40 @@
-import java.io.*;
 import java.util.*;
 public class PositionalConstraintChecker {
 	
 		  
-	   public static ArrayList ConstraintChecker(String[] wordList, String constraint) {
+	   public static ArrayList<String> ConstraintChecker(String[] wordList, String constraint) {
 		  
 		   char letter;
 		   int position;
-		   boolean failed = false;
-		   List validWords = new ArrayList();
-		   for (int i = 0;i < wordList.length; i++) {
-			  
+		   boolean failed;
+		   ArrayList<String> validWords = new ArrayList<String>();
+		   for (String word: wordList) {
+			  failed = false;
 			   for (int j = 0;j < constraint.length();j += 2){
 				   letter = constraint.charAt(j);
-				   position = constraint.charAt(j + 1);
-				   if (letter != wordList[i].charAt(position)){
+				   position = constraint.charAt(j + 1)-1;
+				   
+				   if (position>word.length()||letter != word.charAt(position)){
 					   failed = true;
 					   break;
 				   }
 			   }
-			   if (failed != true){
-				   validWords.add(wordList[i]);
+			   if (!failed){
+				   validWords.add(word);
 			   }
+
 			   
 		  }
 		   return validWords;
-	   
-		
-	}
 	   }
+	   
+	   public static void main(String[] args) {
+		   String[] test = {"Hi","Hello","Beast","parade"};
+		   String constraint = "H0i1";
+		   ArrayList<String> testarray = new ArrayList<String>();
+		   testarray = PositionalConstraintChecker.ConstraintChecker(test,constraint);
+		   for (String s: testarray)
+		   System.out.println(s);
+		   
+		}
+	}
