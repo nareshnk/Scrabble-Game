c10f11e52168b172ea8b65c09867fa04953faae7
unknown <test@L-MAA-25004245.paypalcorp.com>
2015-07-25 08:54:43 +0530

    rearranged the order of methods for Readability

--- BaseWordGenerator.java
+++ BaseWordGenerator.java
@@ -3,90 +3,10 @@ import java.util.*;
 class BaseWordGenerator{
 	
 	private static final String FILE = "C:/Users/test/Documents/ScrabbleWordGenerator/sowpods.txt";
-
-	static Anagrams AnagramCollection;
-
-	public static void addToHash(Map<Integer, String> scoreMap, ArrayList<String> words){
-
-		int score;
-		for(String eachWord: words){
-			if (AnagramCollection.map.containsKey(eachWord)) {
-				score = wordScore(eachWord);
-
-				if (scoreMap.containsKey(score)) {
-					scoreMap.put(score, scoreMap.get(score) + " " + AnagramCollection.map.get(eachWord));
-				}
-				else {
-					scoreMap.put(score, AnagramCollection.map.get(eachWord));
-				}
-			}
-		}
-
-	}
-
 	public static int[] letterScore = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
-
-	public static int wordScore(String word) {
-		int score=0;
-		for(int i = 0; i < word.length(); i++ ) {
-			score += letterScore[word.charAt(i)-'a'];
-		}
-		return score;
-	}
-
-	public static ArrayList<String> generateAllCombinationsOfWords(String input) {
-		char[] inputArray = input.toCharArray();
-		Arrays.sort(inputArray);
-		ArrayList<String> output = new ArrayList<String>();
-
-		for(int i = 2; i <= inputArray.length; i++) {
-			char[] data = new char[i];
-			combinationUtil(inputArray, data, 0, inputArray.length - 1, 0, i, output);
-		}
-		return output;
-	}
-
-	public static Map<Integer, String> generateScoreHash(String rack){
-
-		Map<Integer, String> scoreMap = new HashMap<Integer, String>();
-
-		ArrayList<String> allPossibleWords = (generateAllCombinationsOfWords(rack));
-		addToHash(scoreMap,allPossibleWords);
-		return scoreMap;
-	}
-
-	public static void combinationUtil(char[] arr, char[] data, int start, int end, int index, int r, ArrayList<String> listOfWords) {
-		if (index == r) {
-			listOfWords.add(new String(data));
-			return;
-		}
-
-		for (int i=start; i<=end && end-i+1 >= r-index; i++) {
-			data[index] = arr[i];
-			combinationUtil(arr, data, i+1, end, index+1, r, listOfWords);
-		}
-	}
-
-
-	public static boolean containsWord(String sameWeightWords, String comparionWord){
-
-		boolean result=false;
-
-		if(sameWeightWords!=null)
-		{
-			String [] words = sameWeightWords.split(" ");
-
-			for(String word: words) {
-				if (comparionWord.trim().equals(word.trim())) {
-					return true;
-				}
-			}
-		}
-		return result;
-
-	}
-
-public static void main (String[] args) throws Exception {
+	public static Anagrams AnagramCollection;
+	
+	public static void main (String[] args) throws Exception {
 
 		AnagramCollection = new Anagrams();
         AnagramCollection.readFile(FILE);
@@ -133,9 +53,86 @@ public static void main (String[] args) throws Exception {
 			sortedWords = new TreeMap<Integer, String>(PositionalConstraintChecker.ConstraintChecker(new TreeMap<Integer, String>(sortedWords), constraint));
 		}	
 
-	for(int entry: sortedWords.keySet()) {
-		System.out.println(entry+" "+sortedWords.get(entry));
+		for(int entry: sortedWords.keySet()) {
+			System.out.println(entry+" "+sortedWords.get(entry));
+		}
 	}
+
+	public static ArrayList<String> generateAllCombinationsOfWords(String input) {
+		char[] inputArray = input.toCharArray();
+		Arrays.sort(inputArray);
+		ArrayList<String> output = new ArrayList<String>();
+
+		for(int i = 2; i <= inputArray.length; i++) {
+			char[] data = new char[i];
+			combinationUtil(inputArray, data, 0, inputArray.length - 1, 0, i, output);
+		}
+		return output;
+	}
+
+	public static void combinationUtil(char[] arr, char[] data, int start, int end, int index, int r, ArrayList<String> listOfWords) {
+		if (index == r) {
+			listOfWords.add(new String(data));
+			return;
+		}
+
+		for (int i=start; i<=end && end-i+1 >= r-index; i++) {
+			data[index] = arr[i];
+			combinationUtil(arr, data, i+1, end, index+1, r, listOfWords);
+		}
+	}
+
+	public static int wordScore(String word) {
+		int score=0;
+		for(int i = 0; i < word.length(); i++ ) {
+			score += letterScore[word.charAt(i)-'a'];
+		}
+		return score;
+	}
+		
+	public static Map<Integer, String> generateScoreHash(String rack){
+
+		Map<Integer, String> scoreMap = new HashMap<Integer, String>();
+
+		ArrayList<String> allPossibleWords = (generateAllCombinationsOfWords(rack));
+		addToHash(scoreMap,allPossibleWords);
+		return scoreMap;
+	}
+	
+	public static void addToHash(Map<Integer, String> scoreMap, ArrayList<String> words){
+
+		int score;
+		for(String eachWord: words){
+			if (AnagramCollection.map.containsKey(eachWord)) {
+				score = wordScore(eachWord);
+
+				if (scoreMap.containsKey(score)) {
+					scoreMap.put(score, scoreMap.get(score) + " " + AnagramCollection.map.get(eachWord));
+				}
+				else {
+					scoreMap.put(score, AnagramCollection.map.get(eachWord));
+				}
+			}
+		}
+
+	}
+
+	public static boolean containsWord(String sameWeightWords, String comparionWord){
+
+		boolean result=false;
+
+		if(sameWeightWords!=null)
+		{
+			String [] words = sameWeightWords.split(" ");
+
+			for(String word: words) {
+				if (comparionWord.trim().equals(word.trim())) {
+					return true;
+				}
+			}
+		}
+		return result;
+
 	}
 
 }
